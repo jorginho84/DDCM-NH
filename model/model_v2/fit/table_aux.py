01b69b1fc89f17e29d11a5d396004b1968442028
jorginho84 <jp.rodriguez.osorio@gmail.com>
2017-03-21 13:19:00 -0500

    Adjusted for new model

--- model/model_v2/fit/table_aux.py
+++ model/model_v2/fit/table_aux.py
@@ -30,9 +30,9 @@ ind = ind + 6
 
 #A. t=2, kappas
 for c in range(4):
-	sim_moment = ws.cell('B' + str(c + 11))
-	obs_moment = ws.cell('D' + str(c + 11))
-	obs_sigma = ws.cell('F' + str(c + 11))
+	sim_moment = ws.cell('B' + str(c + 12))
+	obs_moment = ws.cell('D' + str(c + 12))
+	obs_sigma = ws.cell('F' + str(c + 12))
 	sim_moment.value = np.float(beta_kappas_t2[c])
 	obs_moment.value = np.float(moments_vector[ind + c,0])
 	obs_sigma.value = np.float(se_vector[ind + c])
@@ -41,9 +41,9 @@ for c in range(4):
 ind = ind + 4
 #B. t=5, kappas
 for c in range(4):
-	sim_moment = ws.cell('B' + str(c + 15))
-	obs_moment = ws.cell('D' + str(c + 15))
-	obs_sigma = ws.cell('F' + str(c + 15))
+	sim_moment = ws.cell('B' + str(c + 16))
+	obs_moment = ws.cell('D' + str(c + 16))
+	obs_sigma = ws.cell('F' + str(c + 16))
 	sim_moment.value = np.float(beta_kappas_t5[c])
 	obs_moment.value = np.float(moments_vector[ind + c,0])
 	obs_sigma.value = np.float(se_vector[ind + c])
@@ -58,26 +58,26 @@ list_sig = [se_vector[ind:ind + 2], se_vector[ind+2: ind + 5]]
 
 for j in range(2):
 	if j==0:
-		pos = 19
-	else:
-		pos = 21
-	
-	for c in range(2): # 3 moments each
-		sim_moment = ws.cell('B' + str(c + pos))
-		obs_moment = ws.cell('D' + str(c + pos))
-		obs_sigma = ws.cell('F' + str(c + pos))
-		sim_moment.value = np.float(list_aux[j][c])
-		obs_moment.value = np.float(list_obs[j][c])
-		obs_sigma.value = np.float(list_sig[j][c])
-	
+		pos = 20
+		for c in range(2): 
+			sim_moment = ws.cell('B' + str(c + pos))
+			obs_moment = ws.cell('D' + str(c + pos))
+			obs_sigma = ws.cell('F' + str(c + pos))
+			sim_moment.value = np.float(list_aux[j][c])
+			obs_moment.value = np.float(list_obs[j][c])
+			obs_sigma.value = np.float(list_sig[j][c])
+		
 	#the last one identified tfp
-	if j==1:
-		sim_moment = ws.cell('B' + str(23))
-		obs_moment = ws.cell('D' + str(23))
-		obs_sigma = ws.cell('F' + str(23))
-		sim_moment.value = np.float(list_aux[j][2])
-		obs_moment.value = np.float(list_obs[j][2])
-		obs_sigma.value = np.float(list_sig[j][2])
+	else:
+		pos = 22
+			
+		for c in range(3):
+			sim_moment = ws.cell('B' + str(c + pos))
+			obs_moment = ws.cell('D' + str(c + pos))
+			obs_sigma = ws.cell('F' + str(c + pos))
+			sim_moment.value = np.float(list_aux[j][c])
+			obs_moment.value = np.float(list_obs[j][c])
+			obs_sigma.value = np.float(list_sig[j][c])
 
 
 

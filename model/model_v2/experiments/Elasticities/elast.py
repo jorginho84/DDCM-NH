c24797725c2470048f07a616eee2ec5bfaff8600
jorginho84 <jp.rodriguez.osorio@gmail.com>
2017-12-20 17:09:16 -0600

    Computing SEs of elasticities

--- model/model_v2/experiments/Elasticities/elast.py
+++ model/model_v2/experiments/Elasticities/elast.py
@@ -34,6 +34,7 @@ from shock import Shock
 np.random.seed(1)
 
 betas_nelder=np.load('/mnt/Research/nealresearch/new-hope-secure/newhopemount/results/betas_modelv16.npy')
+var_cov=np.load('/mnt/Research/nealresearch/new-hope-secure/newhopemount/results/Model/estimation/sesv2_modelv16_1pc.npy')
 
 #Number of periods where all children are less than or equal to 18
 nperiods = 8
@@ -165,77 +166,175 @@ wr=1
 cs=1
 ws=1
 
-#The estimate class
-output_ins=estimate.Estimate(nperiods,param0,x_w,x_m,x_k,x_wmk,passign,agech0,nkids0,
-	married0,D,dict_grid,M,N,moments_vector,var_cov,hours_p,hours_f,
-	wr,cs,ws)
-
-hours = np.zeros(N)
-childcare  = np.zeros(N)
-
-model_orig  = util.Utility(param0,N,x_w,x_m,x_k,passign,
-	nkids0,married0,hours,childcare,agech0,hours_p,hours_f,wr,cs,ws)
-
-#Obtaining emax instance: this is fixed throughout the exercise
-emax_instance = output_ins.emax(param0,model_orig)
 
 
 
 #Two models: one with shock = 0 and other where shock>0
 shocks = [0,0.01]
-choices_c = {}
-models = []
-for j in range(2):
-	np.save('/mnt/Research/nealresearch/new-hope-secure/newhopemount/results/Model/experiments/NH/shock.npy',shocks[j])
-	models.append(Shock(param0,N,x_w,x_m,x_k,passign,
-		nkids0,married0,hours,childcare,agech0,hours_p,hours_f,wr,cs,ws))
-	choices_c['Choice_' + str(j)] = output_ins.samples(param0,emax_instance,models[j])
 
 
-#Computing changes in % employment for control group
-h_sim_matrix = []
-employment = []
-wages = []
-full = []
-for j in range(2):
-	h_sim_matrix.append(choices_c['Choice_' + str(j)]['hours_matrix'])
-	employment.append(choices_c['Choice_' + str(j)]['hours_matrix']>0)
-	full.append(choices_c['Choice_' + str(j)]['hours_matrix']==hours_f)
-	wages.append(choices_c['Choice_' + str(j)]['wage_matrix'])
+
+###################################################
+##Assuming no estimation errors###
+
+def elast_gen(bs,shocks):
+
+	eta=bs[0]
+	alphap=bs[1]
+	alphaf=bs[2]
+	
+	#wage process
+	wagep_betas=np.array([bs[3],bs[4],bs[5],bs[6],
+		bs[7],bs[8],bs[9]]).reshape((7,1))
+
+	#Production function [young[cc0,cc1],old]
+	gamma1=bs[10]
+	gamma2=bs[11]
+	gamma3=bs[12]
+	tfp=bs[13]
+	
+	kappas=[[bs[14],bs[15],bs[16],bs[17]],[bs[18],bs[19],bs[20],bs[21]]]
+
+	rho_theta_epsilon =  bs[22]
+
+	lambdas=[1,1]
+
+	#Re-defines the instance with parameters 
+	param=util.Parameters(alphap,alphaf,eta,gamma1,gamma2,gamma3,
+		tfp,sigma2theta,rho_theta_epsilon,wagep_betas, marriagep_betas,
+		kidsp_betas, eitc_list,afdc_list,snap_list,cpi,lambdas,kappas,
+		pafdc,psnap,mup)
 
 
+	#The estimate class
+	output_ins=estimate.Estimate(nperiods,param,x_w,x_m,x_k,x_wmk,passign,agech0,nkids0,
+		married0,D,dict_grid,M,N,moments_vector,var_cov,hours_p,hours_f,
+		wr,cs,ws)
+
+	hours = np.zeros(N)
+	childcare  = np.zeros(N)
+
+	model_orig  = util.Utility(param,N,x_w,x_m,x_k,passign,
+		nkids0,married0,hours,childcare,agech0,hours_p,hours_f,wr,cs,ws)
+
+	#Obtaining emax instance: this is fixed throughout the exercise
+	emax_instance = output_ins.emax(param,model_orig)
+
+	choices_c = {}
+	models = []
+	for j in range(2):
+		np.save('/mnt/Research/nealresearch/new-hope-secure/newhopemount/results/Model/experiments/NH/shock.npy',shocks[j])
+		models.append(Shock(param,N,x_w,x_m,x_k,passign,
+			nkids0,married0,hours,childcare,agech0,hours_p,hours_f,wr,cs,ws))
+		choices_c['Choice_' + str(j)] = output_ins.samples(param,emax_instance,models[j])
+
+
+	#Computing changes in % employment for control group
+	h_sim_matrix = []
+	employment = []
+	wages = []
+	full = []
+	for j in range(2):
+		h_sim_matrix.append(choices_c['Choice_' + str(j)]['hours_matrix'])
+		employment.append(choices_c['Choice_' + str(j)]['hours_matrix']>0)
+		full.append(choices_c['Choice_' + str(j)]['hours_matrix']==hours_f)
+		wages.append(choices_c['Choice_' + str(j)]['wage_matrix'])
+
+
+	#Extensive margin
+	elast_extensive = np.zeros(M)
+	for j in range(M):
+		elast_periods = np.zeros(nperiods)
+
+		for t in range(nperiods):
+			sample = (passign[:,0]==0)
+			elast_periods[t] = np.mean((employment[1][sample,t,j] - employment[0][sample,t,j]),axis=0)/(shocks[1]*np.mean((employment[0][sample,t,j]),axis=0))
+		
+		elast_extensive[j] = np.mean(elast_periods)
+
+
+	#Intensive margin
+	elast_intensive = np.zeros(M)
+	for j in range(M):
+		elast_periods = np.zeros(nperiods)
+
+		for t in range(nperiods):
+			sample = (passign[:,0]==0) 
+			elast_periods[t] = np.mean((full[1][sample,t,j] - full[0][sample,t,j]),axis=0)/(shocks[1]*np.mean((employment[0][sample,t,j]),axis=0))
+		
+		elast_intensive[j] = np.mean(elast_periods)
+
+	return {'Extensive': np.mean(elast_extensive), 'Intensive': np.mean(elast_intensive) }	
+	
 
 
 
 ###################################################
 
-#Extensive margin
-elast_extensive = np.zeros(M)
-for j in range(M):
-	elast_periods = np.zeros(nperiods)
-
-	for t in range(nperiods):
-		sample = (passign[:,0]==0)
-		elast_periods[t] = np.mean((employment[1][sample,t,j] - employment[0][sample,t,j]),axis=0)/(shocks[1]*np.mean((employment[0][sample,t,j]),axis=0))
-	
-	elast_extensive[j] = np.mean(elast_periods)
 
 
-#Intensive margin
-elast_intensive = np.zeros(M)
-for j in range(M):
-	elast_periods = np.zeros(nperiods)
+def partial(psi,eps,S,shocks):
+	"""
+	This function computes gradient of elasticities w/r to parameters
+	S: number of structural parameters
+	psi: structural parameters
+	eps: marginal difference
 
-	for t in range(nperiods):
-		sample = (passign[:,0]==0) 
-		elast_periods[t] = np.mean((full[1][sample,t,j] - full[0][sample,t,j]),axis=0)/(shocks[1]*np.mean((employment[0][sample,t,j]),axis=0))
-	
-	elast_intensive[j] = np.mean(elast_periods)
+	"""
+	#save results here
+	db_dt_extensive = np.zeros(S)
+	db_dt_intensive = np.zeros(S)
 
-np.mean(elast_extensive)
-np.mean(elast_intensive)
-np.std(elast_intensive)
-np.std(elast_extensive)
+	for s in range(S):
+
+		#evaluating at optimum
+		psi_low = psi.copy()
+		psi_high = psi.copy()
+
+		#changing only relevant parameter, one at a time
+		psi_low[s] = psi[s] - eps
+		psi_high[s] = psi[s] + eps
+
+		#Computing elasticities
+		low = elast_gen(psi_low,shocks)
+		high = elast_gen(psi_high,shocks)
+		extensive_low = low['Extensive']
+		extensive_high = high['Extensive']
+		intensive_low = low['Intensive']
+		intensive_high = high['Intensive']
+
+		db_dt_extensive[s] = (extensive_high - extensive_low) / (psi_high[s]-psi_low[s])
+		db_dt_intensive[s] = (intensive_high - intensive_low) / (psi_high[s]-psi_low[s])
+
+	return {'der_extensive': db_dt_extensive, 'der_extensive': db_dt_intensive}
 
 
-###################################################
+#Point estimates
+elas = elast_gen(betas_nelder,shocks)
+
+
+#SEs
+npar = betas_nelder.shape[0]
+derivatives = partial(betas_nelder,0.01,npar,shocks)
+der_extensive = derivatives['derivatives']
+se_extensive = np.sqrt(np.dot(np.transpose(der_extensive),np.dot(var_cov,der_extensive)))
+se_intensive = np.sqrt(np.dot(np.transpose(der_intensive),np.dot(var_cov,der_intensive)))
+
+
+print ''
+print 'Extensive-margin elasticity'
+print ''
+print 'Point estimate: ', elas['Extensive']
+print 'SE: ', se_extensive
+print ''
+print 'Point estimate: ', elas['Intensive']
+print 'SE: ', se_intensive
+print ''
+
+
+
+
+
+
+
+
